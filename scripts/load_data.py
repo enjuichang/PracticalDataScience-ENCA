a6dfab88ec22e8a53e4bdd07eeaaa0c984a73233
enjuichang <55577469+enjuichang@users.noreply.github.com>
2021-11-27 23:46:32 +0000

    Upload model (Cosine Sim Wrong)

--- scripts/load_data.py
+++ scripts/load_data.py
@@ -3,11 +3,11 @@ import json
 import pandas as pd
 
 
-data_path = "data/First_1000.json"
+data_path = "../data/First_1000.json"
 raw_json = json.loads(open(data_path).read())
 
 playlists = raw_json["playlists"]
 df = pd.json_normalize(playlists, 
                 record_path='tracks', 
                 meta=['name'])
-df.to_csv("./data/raw_data.csv")
+df.to_csv("../data/raw_data.csv")
