9cdc69c320953e4902e0b6f347fa3cb524de30b4
Cameron-Watts <cameronwatts@minerva.kgi.edu>
2021-12-14 10:56:59 +0000

    Reload data, fix data generation script

---
+++ notebooks/scripts/load_data.py
@@ -0,0 +1,13 @@
+from pprint import pprint
+import json
+import pandas as pd
+
+
+data_path = "../data/First_1000.json"
+raw_json = json.loads(open(data_path).read())
+
+playlists = raw_json["playlists"]
+df = pd.json_normalize(playlists, 
+                record_path='tracks', 
+                meta=['name'])
+df.to_csv("../data/raw_data.csv")
