d8ff105df4563aab9c3d5dc5a4f41af5f16ff57a
Nazar <yaremko.nazar2000@gmail.com>
2021-12-15 13:36:12 +0100

    Added code for the recommednation app (version 1)

--- recommendation_app/application/routes.py
+++ recommendation_app/application/routes.py
@@ -15,8 +15,8 @@ def recommend():
    URL = request.form['URL']
    df = extract(URL)
    edm_top40 = recommend_from_playlist(songDF, complete_feature_set, df)
-   my_songs = []
    number_of_recs = int(request.form['number-of-recs'])
+   my_songs = []
    for i in range(number_of_recs):
       my_songs.append(str(edm_top40.iloc[i,1]) + ' - '+ '"'+str(edm_top40.iloc[i,4])+'"')
    return render_template('results.html',songs= my_songs)
