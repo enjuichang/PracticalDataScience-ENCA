e12949202645ad8f53d76229a7b329545a6af0b5
Nazar <yaremko.nazar2000@gmail.com>
2021-12-17 15:29:02 +0100

    Added inline comments

--- recommendation_app/application/features.py
+++ recommendation_app/application/features.py
@@ -7,18 +7,21 @@ def extract(URL):
     client_id = "5356afb958c84e71a2c37c43e2a2cbf2" 
     client_secret = "83e531491e9c458ba658ac30c4c56bc0"
 
+    #use the clint secret and id details
     client_credentials_manager = SpotifyClientCredentials(client_id=client_id,client_secret=client_secret)
     sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
-       # the URI is split by ':' to get the username and playlist ID
+    # the URI is split by ':' to get the username and playlist ID
     playlist_id = URL.split("/")[4].split("?")[0]
     playlist_tracks_data = sp.playlist_tracks(playlist_id)
 
+    #lists that will be filled in with features
     playlist_tracks_id = []
     playlist_tracks_titles = []
     playlist_tracks_artists = []
     playlist_tracks_first_artists = []
 
+    #go through the dictionary to extract the data
     for track in playlist_tracks_data['items']:
         playlist_tracks_id.append(track['track']['id'])
         playlist_tracks_titles.append(track['track']['name'])
@@ -29,6 +32,7 @@ def extract(URL):
         playlist_tracks_artists.append(artist_list)
         playlist_tracks_first_artists.append(artist_list[0])
 
+    #create a dataframe
     features = sp.audio_features(playlist_tracks_id)
     features_df = pd.DataFrame(data=features, columns=features[0].keys())
     features_df['title'] = playlist_tracks_titles
